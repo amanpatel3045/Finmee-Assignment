216a255da14d746325373e32601b81521e2b6de1
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2023-10-06 20:08:44 +0530

    home and navbar cmp done

---
+++ Finmee/src/Components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <img src="https://ntwist.com/wp-content/uploads/2021/12/ntwist-logo-dark.png" alt="Icon" />
+        
+
+      </div>
+      <div className="navbar-right">
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/industries">Industries</a>
+          </li>
+          <li>
+            <a href="/ai-software">AI Software</a>
+          </li>
+          <li>
+            <a href="/blog">Blog</a>
+          </li>
+          <li>
+            <a href="/contact-us">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

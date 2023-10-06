c915dd7e6c406b14e2e6ca78e862db0b2a808a26
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2023-10-06 20:09:44 +0530

    Footer section completed

---
+++ Finmee/src/Components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Footer.css"
+const Footer = () => {
+  return (
+    <>
+    <div id="foot">
+    <img src="https://ntwist.com/wp-content/uploads/2021/12/ntwistlight.png"
+     alt="image"/>
+     
+     <div className="para">
+     <p>Home</p>
+     <p>About Us</p>
+     <p>Contact Us</p>
+     <p>Privacy Policy</p>
+     <p>Sitemap</p>
+     </div>
+     <p className="grey">9650 20 Ave NW, Edmonton, AB, T6N 1G1, Canada</p>
+     <p className="grey">© 2022. Ntwist Inc.</p>
+    </div>
+    
+    </>
+  );
+};
+
+export default Footer;
+
+
+
+
